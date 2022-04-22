dbf6c952b16dd4bc19496b48a805acfc27a30ce5
Zoe <59600387+zoeschindler@users.noreply.github.com>
2022-04-22 07:49:25 +0200

    Update select_cylinders.m

--- src/tools/select_cylinders.m
+++ src/tools/select_cylinders.m
@@ -1,6 +1,9 @@
 function cylinder = select_cylinders(cylinder,Ind)
 
 Names = fieldnames(cylinder);
+% here >
+Names = Names(Names ~= "CylsInSegment");
+% < here
 n = size(Names,1);
 for i = 1:n
     cylinder.(Names{i}) = cylinder.(Names{i})(Ind,:);
